f6871017666a6799c46cf43cabb83850e8601bb2
floriankramer <flo.kramer@arcor.de>
2019-06-29 17:37:37 +0200

    Added the basics for sorting

--- main.js
+++ main.js
@@ -2489,6 +2489,52 @@ function _Http_track(router, xhr, tracker)
 }
 
 
+function _Time_now(millisToPosix)
+{
+	return _Scheduler_binding(function(callback)
+	{
+		callback(_Scheduler_succeed(millisToPosix(Date.now())));
+	});
+}
+
+var _Time_setInterval = F2(function(interval, task)
+{
+	return _Scheduler_binding(function(callback)
+	{
+		var id = setInterval(function() { _Scheduler_rawSpawn(task); }, interval);
+		return function() { clearInterval(id); };
+	});
+});
+
+function _Time_here()
+{
+	return _Scheduler_binding(function(callback)
+	{
+		callback(_Scheduler_succeed(
+			A2(elm$time$Time$customZone, -(new Date().getTimezoneOffset()), _List_Nil)
+		));
+	});
+}
+
+
+function _Time_getZoneName()
+{
+	return _Scheduler_binding(function(callback)
+	{
+		try
+		{
+			var name = elm$time$Time$Name(Intl.DateTimeFormat().resolvedOptions().timeZone);
+		}
+		catch (e)
+		{
+			var name = elm$time$Time$Offset(new Date().getTimezoneOffset());
+		}
+		callback(_Scheduler_succeed(name));
+	});
+}
+
+
+
 
 // HELPERS
 
@@ -4981,9 +5027,6 @@ var author$project$Main$locationListDecoder = A2(
 	elm$json$Json$Decode$field,
 	'locations',
 	elm$json$Json$Decode$list(author$project$Main$locationDecoder));
-var elm$core$Basics$identity = function (x) {
-	return x;
-};
 var elm$core$Result$mapError = F2(
 	function (f, result) {
 		if (result.$ === 'Ok') {
@@ -5000,6 +5043,9 @@ var elm$core$Basics$composeR = F3(
 		return g(
 			f(x));
 	});
+var elm$core$Basics$identity = function (x) {
+	return x;
+};
 var elm$core$Dict$RBEmpty_elm_builtin = {$: 'RBEmpty_elm_builtin'};
 var elm$core$Dict$empty = elm$core$Dict$RBEmpty_elm_builtin;
 var elm$core$Basics$compare = _Utils_compare;
@@ -5862,40 +5908,233 @@ var elm$http$Http$get = function (r) {
 	return elm$http$Http$request(
 		{body: elm$http$Http$emptyBody, expect: r.expect, headers: _List_Nil, method: 'GET', timeout: elm$core$Maybe$Nothing, tracker: elm$core$Maybe$Nothing, url: r.url});
 };
+var author$project$Main$jsonDataRequest = elm$http$Http$get(
+	{
+		expect: A2(elm$http$Http$expectJson, author$project$Main$ReceivedLocations, author$project$Main$locationListDecoder),
+		url: 'http://localhost:8000/sample.json'
+	});
 var author$project$Main$init = function (_n0) {
-	return _Utils_Tuple2(
-		author$project$Main$Loading,
-		elm$http$Http$get(
-			{
-				expect: A2(elm$http$Http$expectJson, author$project$Main$ReceivedLocations, author$project$Main$locationListDecoder),
-				url: 'http://localhost:8000/sample.json'
-			}));
+	return _Utils_Tuple2(author$project$Main$Loading, author$project$Main$jsonDataRequest);
 };
-var elm$core$Platform$Sub$batch = _Platform_batch;
-var elm$core$Platform$Sub$none = elm$core$Platform$Sub$batch(_List_Nil);
-var author$project$Main$subscr = function (mdl) {
-	return elm$core$Platform$Sub$none;
+var author$project$Main$Tick = function (a) {
+	return {$: 'Tick', a: a};
 };
-var author$project$Main$Failed = function (a) {
-	return {$: 'Failed', a: a};
-};
-var author$project$Main$Success = function (a) {
-	return {$: 'Success', a: a};
-};
-var elm$core$Platform$Cmd$batch = _Platform_batch;
-var elm$core$Platform$Cmd$none = elm$core$Platform$Cmd$batch(_List_Nil);
-var author$project$Main$update = F2(
-	function (msg, model) {
-		if (msg.a.$ === 'Ok') {
-			var locations = msg.a.a;
-			return _Utils_Tuple2(
-				author$project$Main$Success(locations),
-				elm$core$Platform$Cmd$none);
-		} else {
-			return _Utils_Tuple2(
-				author$project$Main$Failed('An error occurred while loading the json.'),
-				elm$core$Platform$Cmd$none);
+var elm$time$Time$Every = F2(
+	function (a, b) {
+		return {$: 'Every', a: a, b: b};
+	});
+var elm$time$Time$State = F2(
+	function (taggers, processes) {
+		return {processes: processes, taggers: taggers};
+	});
+var elm$time$Time$init = elm$core$Task$succeed(
+	A2(elm$time$Time$State, elm$core$Dict$empty, elm$core$Dict$empty));
+var elm$core$Dict$foldl = F3(
+	function (func, acc, dict) {
+		foldl:
+		while (true) {
+			if (dict.$ === 'RBEmpty_elm_builtin') {
+				return acc;
+			} else {
+				var key = dict.b;
+				var value = dict.c;
+				var left = dict.d;
+				var right = dict.e;
+				var $temp$func = func,
+					$temp$acc = A3(
+					func,
+					key,
+					value,
+					A3(elm$core$Dict$foldl, func, acc, left)),
+					$temp$dict = right;
+				func = $temp$func;
+				acc = $temp$acc;
+				dict = $temp$dict;
+				continue foldl;
+			}
 		}
+	});
+var elm$core$Dict$merge = F6(
+	function (leftStep, bothStep, rightStep, leftDict, rightDict, initialResult) {
+		var stepState = F3(
+			function (rKey, rValue, _n0) {
+				stepState:
+				while (true) {
+					var list = _n0.a;
+					var result = _n0.b;
+					if (!list.b) {
+						return _Utils_Tuple2(
+							list,
+							A3(rightStep, rKey, rValue, result));
+					} else {
+						var _n2 = list.a;
+						var lKey = _n2.a;
+						var lValue = _n2.b;
+						var rest = list.b;
+						if (_Utils_cmp(lKey, rKey) < 0) {
+							var $temp$rKey = rKey,
+								$temp$rValue = rValue,
+								$temp$_n0 = _Utils_Tuple2(
+								rest,
+								A3(leftStep, lKey, lValue, result));
+							rKey = $temp$rKey;
+							rValue = $temp$rValue;
+							_n0 = $temp$_n0;
+							continue stepState;
+						} else {
+							if (_Utils_cmp(lKey, rKey) > 0) {
+								return _Utils_Tuple2(
+									list,
+									A3(rightStep, rKey, rValue, result));
+							} else {
+								return _Utils_Tuple2(
+									rest,
+									A4(bothStep, lKey, lValue, rValue, result));
+							}
+						}
+					}
+				}
+			});
+		var _n3 = A3(
+			elm$core$Dict$foldl,
+			stepState,
+			_Utils_Tuple2(
+				elm$core$Dict$toList(leftDict),
+				initialResult),
+			rightDict);
+		var leftovers = _n3.a;
+		var intermediateResult = _n3.b;
+		return A3(
+			elm$core$List$foldl,
+			F2(
+				function (_n4, result) {
+					var k = _n4.a;
+					var v = _n4.b;
+					return A3(leftStep, k, v, result);
+				}),
+			intermediateResult,
+			leftovers);
+	});
+var elm$time$Time$addMySub = F2(
+	function (_n0, state) {
+		var interval = _n0.a;
+		var tagger = _n0.b;
+		var _n1 = A2(elm$core$Dict$get, interval, state);
+		if (_n1.$ === 'Nothing') {
+			return A3(
+				elm$core$Dict$insert,
+				interval,
+				_List_fromArray(
+					[tagger]),
+				state);
+		} else {
+			var taggers = _n1.a;
+			return A3(
+				elm$core$Dict$insert,
+				interval,
+				A2(elm$core$List$cons, tagger, taggers),
+				state);
+		}
+	});
+var elm$time$Time$Name = function (a) {
+	return {$: 'Name', a: a};
+};
+var elm$time$Time$Offset = function (a) {
+	return {$: 'Offset', a: a};
+};
+var elm$time$Time$Zone = F2(
+	function (a, b) {
+		return {$: 'Zone', a: a, b: b};
+	});
+var elm$time$Time$customZone = elm$time$Time$Zone;
+var elm$time$Time$setInterval = _Time_setInterval;
+var elm$time$Time$spawnHelp = F3(
+	function (router, intervals, processes) {
+		if (!intervals.b) {
+			return elm$core$Task$succeed(processes);
+		} else {
+			var interval = intervals.a;
+			var rest = intervals.b;
+			var spawnTimer = elm$core$Process$spawn(
+				A2(
+					elm$time$Time$setInterval,
+					interval,
+					A2(elm$core$Platform$sendToSelf, router, interval)));
+			var spawnRest = function (id) {
+				return A3(
+					elm$time$Time$spawnHelp,
+					router,
+					rest,
+					A3(elm$core$Dict$insert, interval, id, processes));
+			};
+			return A2(elm$core$Task$andThen, spawnRest, spawnTimer);
+		}
+	});
+var elm$time$Time$onEffects = F3(
+	function (router, subs, _n0) {
+		var processes = _n0.processes;
+		var rightStep = F3(
+			function (_n6, id, _n7) {
+				var spawns = _n7.a;
+				var existing = _n7.b;
+				var kills = _n7.c;
+				return _Utils_Tuple3(
+					spawns,
+					existing,
+					A2(
+						elm$core$Task$andThen,
+						function (_n5) {
+							return kills;
+						},
+						elm$core$Process$kill(id)));
+			});
+		var newTaggers = A3(elm$core$List$foldl, elm$time$Time$addMySub, elm$core$Dict$empty, subs);
+		var leftStep = F3(
+			function (interval, taggers, _n4) {
+				var spawns = _n4.a;
+				var existing = _n4.b;
+				var kills = _n4.c;
+				return _Utils_Tuple3(
+					A2(elm$core$List$cons, interval, spawns),
+					existing,
+					kills);
+			});
+		var bothStep = F4(
+			function (interval, taggers, id, _n3) {
+				var spawns = _n3.a;
+				var existing = _n3.b;
+				var kills = _n3.c;
+				return _Utils_Tuple3(
+					spawns,
+					A3(elm$core$Dict$insert, interval, id, existing),
+					kills);
+			});
+		var _n1 = A6(
+			elm$core$Dict$merge,
+			leftStep,
+			bothStep,
+			rightStep,
+			newTaggers,
+			processes,
+			_Utils_Tuple3(
+				_List_Nil,
+				elm$core$Dict$empty,
+				elm$core$Task$succeed(_Utils_Tuple0)));
+		var spawnList = _n1.a;
+		var existingDict = _n1.b;
+		var killTask = _n1.c;
+		return A2(
+			elm$core$Task$andThen,
+			function (newProcesses) {
+				return elm$core$Task$succeed(
+					A2(elm$time$Time$State, newTaggers, newProcesses));
+			},
+			A2(
+				elm$core$Task$andThen,
+				function (_n2) {
+					return A3(elm$time$Time$spawnHelp, router, spawnList, existingDict);
+				},
+				killTask));
 	});
 var elm$core$List$map = F2(
 	function (f, xs) {
@@ -5910,6 +6149,90 @@ var elm$core$List$map = F2(
 				}),
 			_List_Nil,
 			xs);
+	});
+var elm$time$Time$Posix = function (a) {
+	return {$: 'Posix', a: a};
+};
+var elm$time$Time$millisToPosix = elm$time$Time$Posix;
+var elm$time$Time$now = _Time_now(elm$time$Time$millisToPosix);
+var elm$time$Time$onSelfMsg = F3(
+	function (router, interval, state) {
+		var _n0 = A2(elm$core$Dict$get, interval, state.taggers);
+		if (_n0.$ === 'Nothing') {
+			return elm$core$Task$succeed(state);
+		} else {
+			var taggers = _n0.a;
+			var tellTaggers = function (time) {
+				return elm$core$Task$sequence(
+					A2(
+						elm$core$List$map,
+						function (tagger) {
+							return A2(
+								elm$core$Platform$sendToApp,
+								router,
+								tagger(time));
+						},
+						taggers));
+			};
+			return A2(
+				elm$core$Task$andThen,
+				function (_n1) {
+					return elm$core$Task$succeed(state);
+				},
+				A2(elm$core$Task$andThen, tellTaggers, elm$time$Time$now));
+		}
+	});
+var elm$core$Basics$composeL = F3(
+	function (g, f, x) {
+		return g(
+			f(x));
+	});
+var elm$time$Time$subMap = F2(
+	function (f, _n0) {
+		var interval = _n0.a;
+		var tagger = _n0.b;
+		return A2(
+			elm$time$Time$Every,
+			interval,
+			A2(elm$core$Basics$composeL, f, tagger));
+	});
+_Platform_effectManagers['Time'] = _Platform_createManager(elm$time$Time$init, elm$time$Time$onEffects, elm$time$Time$onSelfMsg, 0, elm$time$Time$subMap);
+var elm$time$Time$subscription = _Platform_leaf('Time');
+var elm$time$Time$every = F2(
+	function (interval, tagger) {
+		return elm$time$Time$subscription(
+			A2(elm$time$Time$Every, interval, tagger));
+	});
+var author$project$Main$subscr = function (mdl) {
+	return A2(elm$time$Time$every, 15, author$project$Main$Tick);
+};
+var author$project$Main$Failed = function (a) {
+	return {$: 'Failed', a: a};
+};
+var author$project$Main$Pressure = {$: 'Pressure'};
+var author$project$Main$Success = function (a) {
+	return {$: 'Success', a: a};
+};
+var elm$core$Platform$Cmd$batch = _Platform_batch;
+var elm$core$Platform$Cmd$none = elm$core$Platform$Cmd$batch(_List_Nil);
+var author$project$Main$update = F2(
+	function (msg, model) {
+		if (msg.$ === 'ReceivedLocations') {
+			if (msg.a.$ === 'Ok') {
+				var locations = msg.a.a;
+				return _Utils_Tuple2(
+					author$project$Main$Success(
+						{locations: locations, order: author$project$Main$Pressure}),
+					elm$core$Platform$Cmd$none);
+			} else {
+				return _Utils_Tuple2(
+					author$project$Main$Failed('An error occurred while loading the json.'),
+					elm$core$Platform$Cmd$none);
+			}
+		} else {
+			var time = msg.a;
+			return _Utils_Tuple2(model, author$project$Main$jsonDataRequest);
+		}
 	});
 var elm$json$Json$Decode$map = _Json_map1;
 var elm$json$Json$Decode$map2 = _Json_map2;
@@ -6027,6 +6350,25 @@ var author$project$Main$locationToHtml = function (loc) {
 					]))
 			]));
 };
+var elm$core$List$sortBy = _List_sortBy;
+var author$project$Main$sortLocations = function (locdata) {
+	var _n0 = locdata.order;
+	if (_n0.$ === 'Alphabetic') {
+		return A2(
+			elm$core$List$sortBy,
+			function ($) {
+				return $.name;
+			},
+			locdata.locations);
+	} else {
+		return A2(
+			elm$core$List$sortBy,
+			function ($) {
+				return $.pressure;
+			},
+			locdata.locations);
+	}
+};
 var elm$core$List$append = F2(
 	function (xs, ys) {
 		if (!ys.b) {
@@ -6055,7 +6397,7 @@ var author$project$Main$view = function (model) {
 						elm$html$Html$text(msg)
 					]));
 		default:
-			var locations = model.a;
+			var data = model.a;
 			return A2(
 				elm$html$Html$div,
 				_List_fromArray(
@@ -6066,7 +6408,10 @@ var author$project$Main$view = function (model) {
 					elm$core$List$append,
 					_List_fromArray(
 						[author$project$Main$locationHeaders]),
-					A2(elm$core$List$map, author$project$Main$locationToHtml, locations)));
+					A2(
+						elm$core$List$map,
+						author$project$Main$locationToHtml,
+						author$project$Main$sortLocations(data))));
 	}
 };
 var elm$browser$Browser$External = function (a) {
--- src/main.elm
+++ src/main.elm
@@ -3,50 +3,60 @@ import Html
 import Html.Attributes as HTMLATTR
 import Http
 import Json.Decode as JSD
+import Time
 -- import Json
 
 
 main = Browser.element {init = init, update = update, view = view, subscriptions = subscr}
 
 -- MODEL
+type Order = Alphabetic | Pressure
 type alias Location = {name : String, pressure : Float, imgurl : String}
 
 locationToString : Location -> String
 locationToString loc = 
   loc.name ++ " " ++ String.fromFloat loc.pressure
 
-type Model = Loading | Failed String | Success (List Location)
-init : () -> (Model, Cmd Msg)
-init _ = (Loading, Http.get
+type alias LocData = {locations : (List Location), order : Order}
+
+jsonDataRequest = Http.get
       { url = "http://localhost:8000/sample.json"
       , expect = Http.expectJson ReceivedLocations locationListDecoder  
-      })
+      }
+
+type Model = Loading | Failed String | Success LocData 
+init : () -> (Model, Cmd Msg)
+init _ = (Loading, jsonDataRequest)
 
 
 -- UPDATE
-type Msg = ReceivedLocations (Result Http.Error (List Location))
+type Msg = ReceivedLocations (Result Http.Error (List Location)) | Tick Time.Posix 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     ReceivedLocations (Ok locations) ->
-          (Success locations, Cmd.none)
+          (Success {locations = locations, order = Pressure}, Cmd.none)
     ReceivedLocations (Err _) ->
           (Failed "An error occurred while loading the json.", Cmd.none)
+    Tick time ->
+      (model, jsonDataRequest)
 
 
 -- SUBSCRIPTIONS
 subscr: Model -> Sub Msg
-subscr mdl = Sub.none 
+subscr mdl = 
+  Time.every 15 Tick
 
 
 -- VIEW
-
 locationToHtml : Location -> Html.Html Msg
 locationToHtml loc =
   Html.div [HTMLATTR.class "loc-row"] [
-    Html.div [HTMLATTR.class "loc-cell-img"] [Html.img [HTMLATTR.attribute "src" loc.imgurl] []],
+    Html.div [HTMLATTR.class "loc-cell-img"]
+             [Html.img [HTMLATTR.attribute "src" loc.imgurl] []],
     Html.div [HTMLATTR.class "loc-cell-name"] [Html.text loc.name],
-    Html.div [HTMLATTR.class "loc-cell-pressure"] [Html.text (String.fromFloat loc.pressure)]
+    Html.div [HTMLATTR.class "loc-cell-pressure"]
+             [Html.text (String.fromFloat loc.pressure)]
   ]
 
 locationHeaders : Html.Html Msg
@@ -61,6 +71,16 @@ locationHeaders =
         ]
     )
 
+sortLocations : LocData -> List Location
+sortLocations locdata =
+  case locdata.order of
+    Alphabetic ->
+      List.sortBy .name locdata.locations  
+    Pressure ->
+      List.sortBy .pressure locdata.locations  
+
+   
+
 view : Model -> Html.Html Msg
 view model =
   case model of
@@ -68,8 +88,9 @@ view model =
       Html.div [] [Html.text "Loading"]
     Failed msg ->
       Html.div [] [Html.text msg]
-    Success locations ->
-      Html.div [HTMLATTR.class "locations"] (List.append [locationHeaders] (List.map locationToHtml locations))
+    Success data ->
+      Html.div [HTMLATTR.class "locations"]
+       (List.append [locationHeaders] (List.map locationToHtml (sortLocations data)))
 
 -- DECODERS
 
@@ -87,5 +108,3 @@ locationDecoder = JSD.map3 Location nameDecoder pressureDecoder imgurlDecoder
 
 locationListDecoder : JSD.Decoder (List Location)
 locationListDecoder = JSD.field "locations" (JSD.list locationDecoder)
-
-
