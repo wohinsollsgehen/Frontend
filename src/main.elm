590c46c7f6e3610cab13e31f5fec2206df0b8905
floriankramer <flo.kramer@arcor.de>
2019-06-30 11:38:34 +0200

    removed the info icon if no info url is present

--- src/main.elm
+++ src/main.elm
@@ -130,7 +130,7 @@ locationToHtml loc =
                            Html.img [attribute "src" "img/info-circle.svg", class "loc-info"] [] 
                          ]
                        Nothing ->
-                         Html.img[attribute "src" "img/info-circle.svg", class "loc-info"] []
+                         Html.img[class "loc-info"] []
                         , 
                      -- Text
                      Html.div [class "loc-description"] [Html.text (Maybe.withDefault "" loc.description)],
